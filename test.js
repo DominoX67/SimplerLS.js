d262b5abc5c02e2a8b608340dbcf59803264d86b
DominoX67 <44480339+DominoX67@users.noreply.github.com>
2019-02-01 13:37:13 -0500

    fixes bug
    bug fix commiting

--- test.js
+++ test.js
@@ -3,14 +3,19 @@ if (typeof localStorage === "undefined" || localStorage === null) {
     localStorage = new LocalStorage('./ls');
 }
 var ls = require("./ls");
-ls.set("a", "b");
-console.log(ls.get("a"));
-var array = ["this", 1, "that"];
-ls.setObj("arr", array);
-console.log(ls.getObj("arr"));
-if (ls.exist("arr")) {
-    ls.remove("arr");
-    console.log(ls.get("arr"));
-}
-ls.clear();
-console.log(ls.get("a"));
+ls.set(localStorage, LocalStorage);/*local storages*/
+/*delete locals torages*/
+ls.remove(localStorage)
+ls.clear(ls.clear())
+/*destory local storages*\/*.local*/
+localStorage = null
+LocalStorage = localStorage/*this make local storages work good*/
+/*bug fix*/
+ls.set(LocalStorage,   localStorage
++ ls.get(LocalStorage) )
+ls.clear ( LocalStorage )
+/*ls clears*/
+LocalStorage = new require('node-localstorage').LocalStorage('ls')
+localstorage - new LocalStrages - 129
+ls.set(ls.set(localstorage, LocalStorage), localStorage)
+/*fixed bug*/
